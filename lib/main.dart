4d312e677cfa431fff997ed7571d1122b0cfbb5a
chudaraj poudyal <crpoudyal@gmail.com>
2020-06-03 19:18:03 +0545

    comments codes deleted

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
@@ -13,134 +14,69 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-     
       title: "Demo APP",
-      theme:ThemeData(
+      theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: homepage(),
     );
-  
   }
 }
+
 class homepage extends StatefulWidget {
   @override
   _homepageState createState() => _homepageState();
 }
 
 class _homepageState extends State<homepage> {
-  int dataToChange=0;
-  
-  void DataChanged(){
+  int dataToChange = 0;
+
+  void DataChanged() {
     setState(() {
-      dataToChange +=1;
+      dataToChange += 1;
     });
-    
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "Demo APP"
-        ),
-        
+        title: Text("Demo APP"),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text("You have Clicked",
-            style: TextStyle(
-              fontSize: 20.0,
-            ),),
-            Text("$dataToChange",
-            style: TextStyle(
-              fontSize: 50.0,
+            Text(
+              "You have Clicked",
+              style: TextStyle(
+                fontSize: 20.0,
+              ),
             ),
+            Text(
+              "$dataToChange",
+              style: TextStyle(
+                fontSize: 50.0,
+              ),
             ),
             Text("times"),
             Padding(
               padding: EdgeInsets.all(20.0),
             ),
-            RaisedButton(onPressed: DataChanged,
-            color: Colors.green,
-            child: Text("click Me -",
-            style:TextStyle(
-              fontSize: 15.0,
-              color: Colors.white,
+            RaisedButton(
+              onPressed: DataChanged,
+              color: Colors.green,
+              child: Text(
+                "click Me -",
+                style: TextStyle(
+                  fontSize: 15.0,
+                  color: Colors.white,
+                ),
+              ),
             ),
-            ),
-            ),
-            
           ],
         ),
-        ),
-      
+      ),
     );
   }
 }
-
-
-
-
-
-
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: "StateLess Widget",
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-
-//       ),
-//     home: Scaffold(
-//     appBar: AppBar(
-//       title: Text(
-//         "StateLess Widget"
-//         ),
-//     ),
-//     body: Center(
-//       child: Column(
-//         mainAxisAlignment: MainAxisAlignment.start,
-//       children: <Widget>[
-//         Padding(
-//           padding: EdgeInsets.all(20.0),
-//           ),
-//         Text(
-//           "Prekshya Poudyal has",
-//         style: new TextStyle(
-//         fontSize: 20.0,
-//           ),
-//           ),
-//           Padding(
-//             padding: EdgeInsets.all(20.0),
-//             ),
-//           Text("data",
-//           style: TextStyle(
-//           fontSize: 40.0,
-//           ),
-//           ),
-//           Padding(
-//             padding:EdgeInsets.all(20.0),
-//              ),
-//           RaisedButton(onPressed: (){},
-//         color: Colors.orange,
-//           child: Text("Click Me",
-//           style:  TextStyle(
-//              color: Colors.white,
-//           ),
-//           ),
-//           ),
-//       ],
-
-//     ),
-//     ),
-//     ),
-    
-//     );
-  
-//   }
-// }
